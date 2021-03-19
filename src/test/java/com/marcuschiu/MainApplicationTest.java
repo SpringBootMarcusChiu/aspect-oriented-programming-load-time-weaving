3ae40b632998ccf2bb215e657ae02c05e3636534
Marcus Chiu <marcuschiu9@gmail.com>
2021-03-19 09:47:40 -0500

    removed web from test

--- src/test/java/com/marcuschiu/MainApplicationTest.java
+++ src/test/java/com/marcuschiu/MainApplicationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = MainApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(classes = MainApplication.class)
 public class MainApplicationTest {
 
     @Autowired
