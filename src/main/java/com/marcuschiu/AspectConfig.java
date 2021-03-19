d45e2636f5dd087766c528c3105e892d476be285
Marcus Chiu <marcuschiu9@gmail.com>
2021-03-19 09:46:57 -0500

    optimized imports

--- src/main/java/com/marcuschiu/AspectConfig.java
+++ src/main/java/com/marcuschiu/AspectConfig.java
@@ -3,7 +3,6 @@ package com.marcuschiu;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.springframework.stereotype.Component;
 
 @Aspect
 //@Component no need for @Component because we are not using Spring AOP but pure AspectJ
--- src/test/java/com/marcuschiu/MainApplicationTest.java
+++ src/test/java/com/marcuschiu/MainApplicationTest.java
@@ -1,21 +1,14 @@
 package com.marcuschiu;
 
-import com.sun.tools.attach.AgentInitializationException;
-import com.sun.tools.attach.AgentLoadException;
-import com.sun.tools.attach.AttachNotSupportedException;
-import com.sun.tools.attach.VirtualMachine;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.io.File;
-import java.io.IOException;
 
 @SpringBootTest(classes = MainApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class MainApplicationTest {
 
     @Autowired
-    private DefaultController defaultController;
+    DefaultController defaultController;
 
     @Test
     public void test() {
